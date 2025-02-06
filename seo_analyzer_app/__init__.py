e7c30b317efda54a0724c4c30e2eb340dc28ac9c
ilovemath25 <w3573y.young@gmail.com>
2025-02-06 16:11:48 +0700

    Add initial implementation of SEO Analyzer app with basic routing and styling

--- seo_analyzer_app/__init__.py
+++ seo_analyzer_app/__init__.py
@@ -2,5 +2,7 @@ from flask import Flask
 
 def create_app():
    app = Flask(__name__)
-   
+   with app.app_context():
+      from .routes.home import home
+      app.register_blueprint(home)
    return app
