9e10e2d2dbec0e0f5d40e33e72c18eb20a6cd9c8
ilovemath25 <w3573y.young@gmail.com>
2025-02-06 10:20:19 +0700

    restructure app

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
-from SEO_Analyzer import create_app
+from seo_analyzer_app import create_app
 app = create_app()
-if __name__ == '__main__': app.run(host='0.0.0.0', debug=True, port=5001)
+if __name__ == '__main__':
+   app.run(host='0.0.0.0', debug=True, port=5001)
