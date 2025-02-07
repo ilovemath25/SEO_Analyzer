cad14b773ed3f8a144fb688f9382c604557c9717
ilovemath25 <w3573y.young@gmail.com>
2025-02-07 10:41:47 +0700

    modify

--- seo_analyzer_app/routes/home.py
+++ seo_analyzer_app/routes/home.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template
+import json
 
 home = Blueprint('home', __name__)
 
 @home.route('/')
 @home.route('/home')
 def index():
-   return render_template('home.html')
+   with open('seo_analyzer_app/data/recent_search.json') as f:
+      recent_searches = json.load(f)
+   return render_template('home.html', recent_searches=recent_searches)
