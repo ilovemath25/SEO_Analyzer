720a2de03ac218e9098a3815e4e05f1407bf4de5
ilovemath25 <w3573y.young@gmail.com>
2025-02-13 12:06:31 +0700

    modify ranking seo

--- seo_analyzer_app/utils/ranking_seo.py
+++ seo_analyzer_app/utils/ranking_seo.py
@@ -6,9 +6,11 @@ from selenium.webdriver.chrome.options import Options
 from sentence_transformers import SentenceTransformer
 from keybert import KeyBERT
 from bs4 import BeautifulSoup
+import requests
 import asyncio
 import os
 import re
+chromedriver_path = ChromeDriverManager().install()
 
 def load_model(path):
    model_path = path
@@ -33,6 +35,12 @@ def extract_url(google_url):
    match = re.search(r"/url\?q=(https?://[^&]+)", google_url)
    return match.group(1) if match else None
 
+def get_keyword_frequency(keyword):
+   url = f"https://suggestqueries.google.com/complete/search?client=firefox&q={keyword}"
+   response = requests.get(url).json()
+   suggested_keywords = response[1]
+   return suggested_keywords.count(keyword)
+ 
 def get_chrome_options():
    options = Options()
    options.add_argument("--headless")
@@ -46,7 +54,7 @@ def get_chrome_options():
    
 def fetch_page(url):
    options = get_chrome_options()
-   driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+   driver = webdriver.Chrome(service=Service(chromedriver_path), options=options)
    driver.get(url)
    WebDriverWait(driver, 15).until(lambda driver: driver.execute_script("return document.readyState") == "complete")
    soup = BeautifulSoup(driver.page_source, "html.parser")
@@ -62,7 +70,8 @@ async def fetch_google_result(keyword):
    loop = asyncio.get_running_loop()
    task1 = loop.run_in_executor(None, fetch_page, f"https://www.google.com/search?q={keyword}")
    task2 = loop.run_in_executor(None, fetch_page, f"https://www.google.com/search?q={keyword}&start=20")
-   results = await asyncio.gather(task1, task2)
+   task3 = loop.run_in_executor(None, fetch_page, f"https://www.google.com/search?q={keyword}&start=30")
+   results = await asyncio.gather(task1, task2, task3)
    return [result for page_results in results for result in page_results]
 
 async def check_rank(keywords, url):
@@ -74,11 +83,11 @@ async def check_rank(keywords, url):
       if any(url in result for result in page_results): position = next((i + 1 for i, result in enumerate(page_results) if url in result), None)
       else: position = None
       positions[keyword] = position
-   return position
+   return positions
 
 def analyze_rank(url):
    options = get_chrome_options()
-   driver = webdriver.Chrome(options=options)
+   driver = webdriver.Chrome(service=Service(chromedriver_path), options=options)
    driver.get(url)
    WebDriverWait(driver, 15).until(lambda driver: driver.execute_script("return document.readyState") == "complete")
    soup = BeautifulSoup(driver.page_source, "html.parser")
@@ -90,8 +99,28 @@ def analyze_rank(url):
    model = load_model("./seo_analyzer_app/models/fine_tuned_seo_model")
    kw_model = KeyBERT(model)
    keywords = get_keyword(text, kw_model, 10)
-   print(keywords)
-   # asyncio.run(check_rank(keywords, url))
-
+   keyword_frequencies = {kw: get_keyword_frequency(kw) for kw in keywords}
+   keywords = sorted(keyword_frequencies.items(), key=lambda x: x[1], reverse=True)
+   keywords = [kw[0] for kw in keywords]
+   positions = asyncio.run(check_rank(keywords, url))
+   notes = []
+   scores = []
+   for keyword, rank in positions.items():
+      if rank is None: score = 0.5
+      elif rank==1: score = 10
+      elif rank<=3: score = 9
+      elif rank<=5: score = 8
+      elif rank<=10: score = 7
+      elif rank<=15: score = 5
+      elif rank<=20: score = 4
+      else: score = 0.5
+      scores.append([keyword, rank, score])
+      if score >= 1: notes.append(f"your website rank #{rank} with keyword '{keyword}'")
+   total_score = (
+      (scores[0][2] + scores[1][2] + scores[2][2]) * 1.20 +
+      (scores[3][2] + scores[4][2] + scores[5][2]) * 1.00 +
+      (scores[6][2] + scores[7][2] + scores[8][2] + scores[9][2]) * 0.92
+   )
+   return [total_score, notes]
 if __name__=='__main__':
-   analyze_rank("https://ilovemath25.github.io")
+   print(analyze_rank("https://ilovemath25.github.io"))
